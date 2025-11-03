27787281fbda7ba58b0ec2d7c10908ab79b6cf39
Stelaryy <163652258+Stelaryy@users.noreply.github.com>
2025-11-03 17:33:14 +0100

    Create Rectangle.h

---
+++ fichier_d'en_tete/Rectangle.h
@@ -0,0 +1,35 @@
+// Auteur : Ahmed Boukra Bettayeb
+// Version : 2.0
+// Date : 14/10/2025
+// Classe Rectangle - Déclaration
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+    static int compteur;
+    static int decrementer;
+    double Longueur;
+    double Largeur;
+
+public:
+    Rectangle(double Long, double Large);
+    ~Rectangle();
+
+    void setLargeur();
+    double getLargeur() const;
+
+    void setLongueur();
+    double getLongueur() const;
+
+    double calculPerimetre() const;
+    double calculSurface() const;
+
+    static int getCompteurRectangle();
+    static void incrementerCompteur();
+    static void decrementerCompteur();
+
+};
