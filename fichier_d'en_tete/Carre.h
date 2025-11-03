ee8c0f033349c3cf8cf2a2fbcdf38e225f90fc79
Stelaryy <163652258+Stelaryy@users.noreply.github.com>
2025-11-03 17:32:59 +0100

    Create Carre.h

---
+++ fichier_d'en_tete/Carre.h
@@ -0,0 +1,36 @@
+// Auteur : Ahmed Boukra Bettayeb
+// Version : 2.0
+// Date : 14/10/2025
+// Classe Carre - Déclaration
+#include "stdafx.h"
+#pragma once
+
+
+class Carre {
+private:
+    //variable
+    static int compteur;
+    static int decrementer;
+    double cote;
+    //compteur
+    static void incrementerCompteur();
+    static void decrementerCompteur();
+
+public:
+    // Constructeur
+    Carre(double cote);
+    ~Carre();
+
+    // Méthodes
+    double perimetre() const;
+    double surface() const;
+    void setCote();  
+    double getCote() const;
+
+    //compteur
+    static int getCompteurcarre();
+    
+};
+
+
+
