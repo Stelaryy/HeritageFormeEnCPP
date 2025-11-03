31056c0ed72d88eda181fed632ca179e9ec6efc8
Stelaryy <163652258+Stelaryy@users.noreply.github.com>
2025-11-03 17:33:11 +0100

    Create Losange.h

---
+++ fichier_d'en_tete/Losange.h
@@ -0,0 +1,42 @@
+// Auteur : Ahmed Boukra Bettayeb
+// Version : 2.0
+// Date : 14/10/2025
+// Classe Losange - Déclaration
+
+#pragma once
+
+class Losange {
+private:
+    static int compteur;
+    static int decrementer;
+    double d1;
+    double d2;
+    double cote;
+    double angle; // en radians
+    static void incrementerCompteur();
+    static void decrementerCompteur();
+    
+
+public:
+    // Constructeurs
+    Losange(bool fromDiagonales); // saisie par diagonales
+    Losange();                    // saisie par côté + angle
+    ~Losange();
+
+    // Méthodes
+    double perimetre() const;
+    double surfaceParDiagonales() const;
+    double surfaceParCoteEtAngle() const;
+
+    // Getters
+    double getCote() const;
+    double getD1() const;
+    double getD2() const;
+    double getAngleDeg() const;
+
+    static int getCompteur();
+    
+};
+
+
+
