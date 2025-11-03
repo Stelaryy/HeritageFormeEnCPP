b2c6f9c558d3c7908f70018e038d78861a7c9d82
Stelaryy <163652258+Stelaryy@users.noreply.github.com>
2025-11-03 17:33:27 +0100

    Create forme.cpp

---
+++ fichier_sources/forme.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "forme.h"
+
+forme::getsurface(){
+    return 0.0;
+}
+
+forme::getperimetre(){
+    return 0.0;
+}
+
+void forme::SaisirDimension(){
+
+}
+
+void forme::SaisirDimension(double _dim1){
+
+}
+
+void forme::SaisirDimension(double _dim1, double _dim2){
+
+}
+
+void forme::SaisirDimension(double _dim1, double _dim2, double _dim3){
+
+}
