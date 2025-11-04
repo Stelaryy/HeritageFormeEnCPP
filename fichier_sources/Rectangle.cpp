f54fac6563bc941473c6a67e4fb49a6a4e1dc123
Stelaryy <163652258+Stelaryy@users.noreply.github.com>
2025-11-04 10:30:31 +0100

    Update Rectangle.cpp

--- fichier_sources/Rectangle.cpp
+++ fichier_sources/Rectangle.cpp
@@ -1,87 +1,99 @@
-// Auteur : Ahmed Boukra Bettayeb
-// Version : 2.0
-// Date : 14/10/2025
-// rectangle - implementation
+//	Auteur : Wilfrid Grassi
+//	Version : 1.1
+//	Date 13/09/2016
+//	Objet : Definition des methodes de la classe Rectangle 
+//			Calcul de Perimetre et de surface
+//			Instanciation statique des objets (pas de pointeurs)
 
 #include "stdafx.h"
 #include "Rectangle.h"
-#include <iostream>
 using namespace std;
-
-//declaration de mon compteur pour rectangle 
-int Rectangle::compteur = 0;
-int Rectangle::decrementer = 0;
-
-int Rectangle::getCompteurRectangle() {
-    return compteur;
-}
-
-void Rectangle::incrementerCompteur() {
-    compteur++;
-}
-
-void Rectangle::decrementerCompteur() {
-    decrementer--;
-}
 
 Rectangle::Rectangle(double Long, double Large)
 {
-    incrementerCompteur();
-    if (Long > 0 && Large > 0) {
-        this->Longueur = Long;
-        this->Largeur = Large;
-    }
-    else {
-        cout << "Valeurs invalides, valeurs par défaut attribuées (1,1)" << endl;
-        this->Longueur = 1;
-        this->Largeur = 1;
-    }
-    cout << "Creation dun objet de type rectangle : " << endl;
-    cout << "Valeur de la longueur = " << this->Longueur << endl;
-    cout << "Valeur de la largeur =  " << this->Largeur << endl;
-    cout << "Nombre de Retangle en memoire : " << compteur << endl;
-        
+	this->setLongueur(Long);
+	this->setLargeur(Large);
 }
 
-Rectangle::~Rectangle() {
-    decrementerCompteur();
-    cout << "destruction dun objet de type Rectangle : " << endl;
-    cout << "Nombre de Rectangle apres suppression : " << compteur << endl;
-}
-
-void Rectangle::setLargeur()
+Rectangle::~Rectangle()
 {
-    double temp;
-    do {
-        cout << "Entrez la largeur (>0) : ";
-        cin >> temp;
-    } while (temp <= 0);
-    cout << "valeur de la longueur reussi" << endl;
-    this->Largeur = temp;
+	cout << "Suppression de la memoire d un objet Rectangle ..." << endl;
 }
 
-double Rectangle::getLargeur() const {
-    return this->Largeur;
-}
-
-void Rectangle::setLongueur()
+double Rectangle::setLongueur(double Long)
 {
-    double temp;
-    do {
-        cout << "Entrez la longueur (>0) : ";
-        cin >> temp;
-    } while (temp <= 0);
-    this->Longueur = temp;
+	this->Longueur = Long;
+	this->CalculerSurface();
+	this->CalculerPerimetre();
 }
 
-double Rectangle::getLongueur() const {
-    return this->Longueur;
+double Rectangle::setLargeur(double Large)
+{
+	this->Largeur = Large;
+	this->CalculerSurface();
+	this->CalculerPerimetre();
 }
 
-double Rectangle::calculPerimetre() const {
-    return 2 * (this->Longueur + this->Largeur);
+void Rectangle::SaisirLongueur()
+{
+	double Long;
+
+	do
+	{
+		cout << "Entrez la longueur du rectangle: " << endl;
+		cin >> Long;
+	} while (Long < 0.0);
+
+	this->Longueur = Long;
+	this->CalculerSurface();
+	this->CalculerPerimetre();
 }
 
-double Rectangle::calculSurface() const {
-    return this->Longueur * this->Largeur;
+void Rectangle::SaisirLargeur()
+{
+	double Large;
+
+	do
+	{
+		cout << "Entrez la largeur du rectangle: " << endl;
+		cin >> Large;
+	} while (Large < 0.0);
+
+	this->Largeur = Large;
+	this->CalculerSurface();
+	this->CalculerPerimetre();
 }
+
+void Rectangle::SaisirDimensions()
+{
+	this->SaisirLongueur();
+	this->SaisirLargeur();
+}
+
+void Rectangle::SaisirDimensions(double _dim1, double _dim2)
+{
+	this->setLongueur(_dim1);
+	this->setLargeur(_dim2);
+}
+
+void Rectangle::CalculerPerimetre()
+{
+	this->Perimetre = (2 * this->Longueur) + (2 * this->Largeur);
+}
+
+void Rectangle::CalculerSurface()
+{
+	this->Surface = this->Longueur * this->Largeur;
+}
+
+double Rectangle::getSurface()
+{
+	return(this->Surface);
+}
+
+double Rectangle::getPerimetre()
+{
+	return(this->Perimetre);
+}
+
+
